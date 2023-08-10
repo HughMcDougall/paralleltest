7108b3d6b07124c0e9e712a999e445ea555d3b7a
HughMcDougall <hughmcdougallemail@gmail.com>
2023-08-10 11:03:18 +1000

    autocommit via bat

---
+++ autocommit.bat
@@ -0,0 +1,3 @@
+wsl git add .
+wsl git commit -m "autocommit via bat"
+wsl push -u origin main
--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# #SBATCH --ntasks-per-node=1 SBATCH --cpus-per-task 1
+#SBATCH --ntasks-per-node=1 
+#SBATCH --cpus-per-task 2
 #SBATCH --mem-per-cpu= 1M
-# #SBATCH --mail-type=BEGIN,END,FAIL
-# #SBATCH --mail-user=hmgetafix@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hmgetafix@gmail.com
 
 # Load the necessary modules
 module load anaconda3/5.2.0
