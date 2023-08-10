497297b49f20144fa5ea9898d00b451f19c5f99c
HughMcDougall <hughmcdougallemail@gmail.com>
2023-08-10 11:00:54 +1000

    run

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task 1
-#SBATCH --mem-per-cpu= 100M
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=hmgetafix@gmail.com
+# #SBATCH --ntasks-per-node=1 SBATCH --cpus-per-task 1
+#SBATCH --mem-per-cpu= 1M
+# #SBATCH --mail-type=BEGIN,END,FAIL
+# #SBATCH --mail-user=hmgetafix@gmail.com
 
 # Load the necessary modules
 module load anaconda3/5.2.0
 eval "$(conda shell.bash hook)"
 conda activate lag_conda
 
-# Run your Python script with $SLURM_ARRAY_TASK_ID as argument
+# Run your Python script
 python example_slurmjob.py
