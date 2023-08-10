8cea5c35ea39e0719264c13b029625c8798f55c4
HughMcDougall <hughmcdougallemail@gmail.com>
2023-08-10 11:33:43 +1000

    autocommit via bat

--- example_slurmjob.py
+++ example_slurmjob.py
@@ -5,7 +5,7 @@ An example of a parallelizable numpyro job for use on slurm
 
 Meant to be used as a test of multi-node usage on the cluster
 
--HM 6/8
+-HM 6/8, cont 10/8
 '''
 
 #===========================
@@ -17,56 +17,58 @@ from jax.random import PRNGKey
 import jax
 import os
 
-#===========================
-print("-"*79)
-print("Doing NumPyro setup")
-      
-rkey = PRNGKey(1)
+    #===========================
+def main():
+    print("-"*79)
+    print("Doing NumPyro setup")
+          
+    rkey = PRNGKey(1)
 
-num_chains = 2
+    num_chains = 2
 
-if __name__ == "__main__":
-    os.environ["XLA_FLAGS"] = (
-        "--xla_force_host_platform_device_count=%i" %num_chains
-        "--xla_cpu_multi_thread_eigen=true"
-    )
-    
-npy.set_platform('cpu')
-npy.set_host_device_count(num_chains)
+    npy.set_platform('cpu')
+    npy.set_host_device_count(num_chains)
 
-print("Starting program with %i cores registering from jax.local_device_count()" %jax.local_device_count())
+    print("Starting program with %i cores registering from jax.local_device_count()" %jax.local_device_count())
 
-#===========================
+    #===========================
 
-DATA = np.loadtxt('./data.dat')
+    DATA = np.loadtxt('./data.dat')
 
-X, Y, E = DATA[:,0],DATA[:,1],DATA[:,2]
+    X, Y, E = DATA[:,0],DATA[:,1],DATA[:,2]
 
-def model(X,Y,E):
-    
-    m = npy.sample('m', npy.distributions.Uniform(-5,5))
-    c = npy.sample('c', npy.distributions.Uniform(-5,5))
-
-    Y_pred = m*X + c
-    with npy.plate('data',len(X)):
-        npy.sample('y', npy.distributions.Normal(Y_pred,E), obs=Y)
+    def model(X,Y,E):
         
+        m = npy.sample('m', npy.distributions.Uniform(-5,5))
+        c = npy.sample('c', npy.distributions.Uniform(-5,5))
 
-#===========================
-print("-"*79)
-print("Doing MCMC")
+        Y_pred = m*X + c
+        with npy.plate('data',len(X)):
+            npy.sample('y', npy.distributions.Normal(Y_pred,E), obs=Y)
+            
 
-sampler = npy.infer.MCMC(npy.infer.NUTS(model=model),
-                         num_warmup = 300,
-                         num_samples = 500,
-                         num_chains = num_chains)
-sampler.run(rkey, X,Y,E)
+    #===========================
+    print("-"*79)
+    print("Doing MCMC")
 
-print("Done")
-print("-"*79)
+    sampler = npy.infer.MCMC(npy.infer.NUTS(model=model),
+                             num_warmup = 300,
+                             num_samples = 500,
+                             num_chains = num_chains)
+    sampler.run(rkey, X,Y,E)
 
-sampler.print_summary()
-print("-"*79)
+    print("Done")
+    print("-"*79)
+
+    sampler.print_summary()
+    print("-"*79)
 
 
 #==========================
+
+if __name__ == "__main__":
+    os.environ["XLA_FLAGS"] = (
+        "--xla_force_host_platform_device_count=2 "
+        "--xla_cpu_multi_thread_eigen=true"
+    )
+    main()
