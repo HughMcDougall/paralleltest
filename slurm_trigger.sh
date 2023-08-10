f01285a9ec81ec59bfe2c0fe64a23a6bc1f19e89
HughMcDougall <hughmcdougallemail@gmail.com>
2023-08-10 11:22:07 +1000

    autocommit via bat

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=numpyroparalleltest
-#SBATCH --nodes = 1
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu= 10M
+#SBATCH --mem-per-cpu=10M
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hmgetafix@gmail.com
 #SBATCH --time=0:05:00
